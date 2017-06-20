20263d3d78323c04f8836807f39f0068b8637a16
k-water <625592890@qq.com>
2017-06-20 15:07:25 +0800

    fix:修复登录跳转问题

--- Admin/src/router/index.js
+++ Admin/src/router/index.js
@@ -17,6 +17,7 @@ const router = new Router({
     }, {
       path: '/',
       component: require('../components/Home.vue'),
+      name: 'ProList',
       meta: {
         requireAuth: true
       },
@@ -26,68 +27,106 @@ const router = new Router({
         component: require('../page/Profession/ProfessionList.vue'),
         meta: {
           requireAuth: true
-        },
+        }
       }, {
         path: '/addprofession',
         name: 'AddProfession',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Profession/AddProfession.vue')
       }, {
         path: '/editProfession/:id',
         name: 'EditProfession',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Profession/EditProfession.vue')
       }, {
         path: '/improvelist',
         name: 'ImproveList',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Profession/ImproveList.vue')
       }, {
         path: '/editimprove/:id',
         name: 'EditImprove',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Profession/EditImprove.vue')
       }, {
         path: '/addimprove',
         name: 'AddImprove',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Profession/AddImprove.vue')
       }, {
         path: '/infoFiveManage',
         name: 'InfoFiveManage',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Info/FiveInfo.vue')
       }, {
         path: '/addInfoFive',
         name: 'AddInfoFive',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Info/AddFiveInfo.vue')
       }, {
         path: '/infoJobManage',
         name: 'InfoJobManage',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Info/JobInfo.vue')
       }, {
         path: '/addJobInfo',
         name: 'AddJobInfo',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Info/AddJobInfo.vue')
       }, {
         path: '/editJobInfo/:id',
         name: 'EditJobInfo',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Info/EditJobInfo.vue')
       }, {
         path: '/editInfoFive/:id',
         name: 'EditInfoFive',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Info/EditFiveInfo.vue')
       }, {
         path: '/entryManage',
         name: 'EntryManage',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Entry/Entry.vue')
       }, {
         path: '/entryManageDetails/:id',
         name: 'EntryManageDetails',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Entry/EntryDetails.vue')
       }, {
         path: '/messageManage',
         name: 'MessageManage',
+        meta: {
+          requireAuth: true
+        },
         component: require('../page/Message/Message.vue')
       }],
-    },
-    {
-      path: '/',
-      redirect: '/login'
     },
     {
       path: '*',
@@ -97,17 +136,18 @@ const router = new Router({
 })
 router.beforeEach((to, from, next) => {
   let token = localStorage.getItem('token')
+  console.log(token)
   if (to.meta.requireAuth) {
     if (token) {
-      next()
+      return next()
     } else {
-      next({
-        path: '/login',
+      return next({
+        name: 'Login',
         query: { redirect: to.fullPath }
       })
     }
   } else {
-    next()
+    return next()
   }
 })
 
